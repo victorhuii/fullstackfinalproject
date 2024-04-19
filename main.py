e8bf448edc914409a847dbdd5fc11967527a9667
Flechazo <77555511+LBF-2004@users.noreply.github.com>
2024-04-18 21:25:17 -0700

    timer

--- main.py
+++ main.py
@@ -2,3 +2,8 @@
 Design doc for focus web app
 functionality: timer, performance summary...
 '''
+
+import time;
+def timer():
+    # https://www.udacity.com/blog/2021/09/create-a-timer-in-python-step-by-step-guide.html
+    return 'this is a timer'
